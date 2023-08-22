b9f9d1e52598cf5fd6d0730e3281ea4ca1335ed0
Lucasmendoonca <lmendonca775@gmail.com>
2023-08-22 09:55:57 -0300

    atualizando o codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import Cabecalho from "./components/Cabecalho";
 
 export default function App() {
   return (
     
     <>
-      <h1>First DEPLOY in to VERCEL</h1>
-      <h2>Vite + React</h2>
-      <p>Coded By - RM99528</p>
-      <div>
-        <p>Turma - 1TDSPL</p>
-        <p>FIAP</p>
-      </div>
+     <div className="container"> </div>
+
+     <Cabecalho/>
+        <section>
+          <h2>Vite + React</h2>
+          <p>Coded By- RM99528</p>
+          <div>
+            <p>Turma - 1TDSPL</p>
+            <p>FIAP</p>
+          </div>
+        </section>
+    
     </>
 
   )
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,12 @@
+
+export default function Cabecalho() {
+  return (
+    
+    <>
+        <header>
+            <h1>First deploy in to VERCEL</h1>
+        </header>
+    </>
+
+  )
+}
